932f0f35cc57617666592646626376dc07915540
Ellison Leão <ellisonleao@gmail.com>
2019-09-11 19:09:25 -0300

    update navbar logo, removing banner buttons

--- src/components/MainBanner.vue
+++ src/components/MainBanner.vue
@@ -4,10 +4,6 @@
       <img class="main-banner__python-sul" src="@/assets/LogoTipoPythonSul2.svg" alt=""/>
       <img class="main-banner__curitiba" src="@/assets/PythonSul2019-BotanicGarden.svg" alt=""/>
     </div>
-    <div class="main-banner__actions">
-      <a class="btn btn--primary" href="https://www.eventbrite.com.br/e/pythonsul-2019-curitiba-tickets-65624868749" target="_blank">Inscrições</a>
-      <p class="btn btn--rdf-secondary">Palestras encerradas</p>
-    </div>
   </div>
 </template>
 
