ed6a9979d840f33c088c12ead210c8d4978d60fb
Ellison Leão <ellisonleao@gmail.com>
2019-09-12 10:39:55 -0300

    fix link

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -9,7 +9,7 @@
       </div>
       <div id="nav" class="nav-bar__links">
         <router-link class="nav-bar__link" to="/">Home</router-link>
-        <router-link class="nav-bar__link" to="/schedule">Programação</router-link>
+        <router-link class="nav-bar__link" to="/schedule/">Programação</router-link>
         <router-link class="nav-bar__link" to="/key-notes/">Keynotes</router-link>
         <router-link class="nav-bar__link" to="/local">Local</router-link>
         <a class="nav-bar__link" href="https://pythonsul.org/documents/plano_de_patrocinio.pdf" target="_blank">Patrocinar /</a>
