d8275806e02484325f16abc63e6e151408feac9d
Ellison Leão <ellisonleao@gmail.com>
2019-09-12 17:26:49 -0300

    deploy

---
+++ precache-manifest.df94e9ac64e743cd6161f7eb360969e0.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca26a70f538577445c751941558f3cd0",
+    "url": "/img/melissa_mendonca.ca26a70f.jpg"
+  },
+  {
+    "revision": "e0d7a5693a9f1842ac2a",
+    "url": "/js/about.145e9d4f.js"
+  },
+  {
+    "revision": "f705ddd8e31f6f550591",
+    "url": "/js/app.51e5988e.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "b433cc06022beabd5a9e",
+    "url": "/js/chunk-vendors.8ee84b2e.js"
+  },
+  {
+    "revision": "cd29336fd69df3770989f2677e102aed",
+    "url": "/img/PythonSul2019-NomedoEvento.cd29336f.svg"
+  },
+  {
+    "revision": "cc3708b4900e1f1331becf63253c9c97",
+    "url": "/img/icon.png"
+  },
+  {
+    "revision": "24b0aa1745bc05dfb8f612cbdb37c436",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d479d2509f5255fb4da6a1a6bf2f576e",
+    "url": "/img/thais_viana.d479d250.jpg"
+  },
+  {
+    "revision": "804bbc4e40d110b40f8bc42ba735acfe",
+    "url": "/img/mario_sergio.804bbc4e.jpg"
+  },
+  {
+    "revision": "f245bb0610a8c90f23979dbdf031f6fb",
+    "url": "/img/pythonsul_icon.f245bb06.png"
+  },
+  {
+    "revision": "a205eea353a0e29f8286a2763a50d041",
+    "url": "/img/unicuritiba.a205eea3.jpg"
+  },
+  {
+    "revision": "a5978091a6e6ca94d3d5df89e8d8f893",
+    "url": "/img/unicuritiba.a5978091.png"
+  },
+  {
+    "revision": "692ebf38d746bad5553713fd41edde77",
+    "url": "/img/pythonsul-logo.692ebf38.png"
+  },
+  {
+    "revision": "51c2eab22c8994e70e7633979982b451",
+    "url": "/img/logo-python.51c2eab2.svg"
+  },
+  {
+    "revision": "067050f6ecc418ea7a7d2f3557683c8c",
+    "url": "/img/1024px-Python-logo-notext.svg.067050f6.png"
+  },
+  {
+    "revision": "8f659a94654f5c1ea135fa82795b788b",
+    "url": "/img/LogoTipoPythonSul2.8f659a94.svg"
+  },
+  {
+    "revision": "79f6afb9feab98acb091721273c0a727",
+    "url": "/img/araucaria.79f6afb9.svg"
+  },
+  {
+    "revision": "3b40aaa5c14a7c47eecf02421b6e275b",
+    "url": "/img/PythonSul2019-JardimBotanico.3b40aaa5.svg"
+  },
+  {
+    "revision": "8999106d7006f53482db7c2ffe75aeae",
+    "url": "/img/araucaria.8999106d.jpg"
+  },
+  {
+    "revision": "682578a99ada237d1b363f760dd3161f",
+    "url": "/img/PythonSul2019-BotanicGarden.682578a9.svg"
+  },
+  {
+    "revision": "788c53dd371f12eae93578381f24e48a",
+    "url": "/img/LogoPythonSul.788c53dd.svg"
+  },
+  {
+    "revision": "642485a632db8d532e1ef80ac9522fb1",
+    "url": "/img/LogoTipoPythonSul.642485a6.svg"
+  },
+  {
+    "revision": "d528558fd915c4ae08ea2516fcbb6ba7",
+    "url": "/img/LogoTipoPythonSul1.d528558f.svg"
+  },
+  {
+    "revision": "ee7dc64be1db60bbe4db4c87769b6f24",
+    "url": "/img/Flux.ee7dc64b.svg"
+  },
+  {
+    "revision": "91a90e09fc61de147e200caa04e63c1d",
+    "url": "/fonts/Flux.91a90e09.otf"
+  },
+  {
+    "revision": "59bbc192525c793bd73d444ffb23a866",
+    "url": "/fonts/Flux.59bbc192.woff"
+  },
+  {
+    "revision": "119fd118fd31150b543266f297283f57",
+    "url": "/fonts/Flux.119fd118.ttf"
+  },
+  {
+    "revision": "01d21412d9986d349b4edbd996b66161",
+    "url": "/fonts/Flux.01d21412.eot"
+  },
+  {
+    "revision": "b433cc06022beabd5a9e",
+    "url": "/css/chunk-vendors.81e18a23.css"
+  },
+  {
+    "revision": "f705ddd8e31f6f550591",
+    "url": "/css/app.d1ea03b8.css"
+  },
+  {
+    "revision": "d9c13bb028051ccb16539482c25534c7",
+    "url": "/CNAME"
+  }
+];
